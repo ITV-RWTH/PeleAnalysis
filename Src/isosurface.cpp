6040fbd91423f02da92546b02cf2cd6997743e83
ng746933 <ng746933@login23-g-1.hpc.itc.rwth-aachen.de>
2025-04-14 21:26:49 +0200

    Added EB capable isosurface Tool

--- Src/isosurface.cpp
+++ Src/isosurface.cpp
@@ -6,6 +6,7 @@
 #include <AMReX_ParmParse.H>
 #include <AMReX_MultiFab.H>
 
+//#include <AMReX_BLFort.H>
 #include <AMReX_PlotFileUtil.H>
 #include <AMReX_FillPatchUtil.H>
 #include "makelevelset3.h"
@@ -1405,6 +1406,9 @@ main(int argc, char* argv[])
     int nodeCtr = 0;
 
     Vector<MultiFab> states(Nlev);
+    #ifdef AMREX_USE_EB
+    Vector<MultiFab> vFdata(Nlev);
+    #endif
     Vector<DistributionMapping> dmaps(Nlev);
 
     for (int lev = 0; lev < Nlev; ++lev) {
@@ -1414,7 +1418,7 @@ main(int argc, char* argv[])
       const Box& gpdomain = geoms[lev].growPeriodicDomain(nGrow[lev]);
 
       // Define data holders
-      dmaps[lev] = DistributionMapping(grids[lev]);
+      dmaps[lev] = pf.DistributionMap(lev); //DistributionMapping(grids[lev]);
       BoxArray gba = BoxArray(grids[lev]).grow(nGrow[lev]);
 
       states[lev].define(
@@ -1424,6 +1428,16 @@ main(int argc, char* argv[])
           new MultiFab(grids[lev], dmaps[lev], 1, nGrow[lev]));
       }
 
+      // Define and load "volFrac" for usage inside the mask 
+      #ifdef AMREX_USE_EB
+        Print() << "Reading volFrac from plotfile data at level " << lev << "..." << std::endl;
+        vFdata[lev].define(grids[lev],dmaps[lev],1,nGrow[lev]);
+        vFdata[lev].setVal(0.0);
+        const MultiFab& src = pf.get(lev, "volFrac");
+        MultiFab::Copy(vFdata[lev], src, 0, 0, 1, 0);
+        vFdata[lev].FillBoundary(geoms[lev].periodicity());
+        Print() << "...done reading the plotfile data at level " << lev << "..." << std::endl;
+      #endif
       // Get cell center coordinates
       const auto geomdata = geoms[lev].data();
 #ifdef AMREX_USE_OMP
@@ -1522,7 +1536,20 @@ main(int argc, char* argv[])
         const auto& gbox = mask.box();
         const auto g1box = grow(mfi.validbox(), 1);
 
-        mask.setVal<amrex::RunOn::Device>(1.0);
+        mask.setVal<amrex::RunOn::Device>(1.0); // Setting mask to level 1 initially
+
+        #ifdef AMREX_USE_EB
+          //Setting Mask for EB to -1 to exclude Cells with volFrac = 0 
+          Array4<Real> const& volFracBox = vFdata[lev].array(mfi);
+          Array4<Real> maskArray = mask.array();
+
+          AMREX_PARALLEL_FOR_3D(gbox, i, j, k, {
+              if(volFracBox(i,j,k) > 1) Print() << "Bigger than 1! volFrac = " << volFracBox(i,j,k) << std::endl; 
+              if (volFracBox(i, j, k) == 0) {
+                  maskArray(i, j, k) = -1.0;
+              }
+          });
+        #endif
 
         if (lev < finestLevel && !build_distance_function) {
           const auto fratio = pf.refRatio(lev);
